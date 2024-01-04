7c66f4c6a877d2ebb2f951101d3b8246dfc9450b
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2024-01-04 13:24:47 -0300

    todo's

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -16,6 +16,11 @@ export const Container = () => {
 	}, []);
 
 	return (
+		// TODO --> Manejar las columnas de forma dinámica, dependiendo del tamaño de la pantalla (col-1, col-2, col-3, col-4)
+		// TODO --> Agregar una card de "Cargando publicaciones" mientras se cargan las publicaciones
+		// TODO --> Agregar un botón de "Cargar publicaciones" cuando no hay publicaciones
+		// TODO --> Manejar el tema de ocupar toda la pantalla y que no haya un espacio en blanco al final
+		
 		<div className="container">
 			<div className="row row-cols-3">
 				{publications.map((pub) => (
